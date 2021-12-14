248bfafc3e3507c0c6acaa01ee8023ef2bb41cb9
JeonChangJun <jcjk0302@gmail.com>
2021-12-14 14:02:33 +0900

    Account permission decorator update

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import NewModel
 
@@ -40,9 +43,23 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
+has_ownership = [account_ownership_required, login_required]
+
+
+@method_decorator(account_ownership_required, 'get')
+@method_decorator(account_ownership_required, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+
+@method_decorator(account_ownership_required, 'get')
+@method_decorator(account_ownership_required, 'post')
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/delete.html'
